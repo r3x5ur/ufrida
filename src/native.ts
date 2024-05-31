018148c518c0eaae8716197d472cef6207cfbd8d
zzdev <zzdev@developer.org>
2024-05-31 16:05:41 +0800

    fix new version error

--- src/native.ts
+++ src/native.ts
@@ -1,6 +1,10 @@
 export const binding = (() => {
   try {
-    return require('../build/Release/frida_binding.node')
+    try {
+      return require('../build/Release/frida_binding.node')
+    } catch (e) {
+      return require('../build/frida_binding.node')
+    }
   } catch (e) {
     console.error('Bindings not found, Please reinstall the package.')
   }
